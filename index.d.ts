83b6cb8aa331aa2c1a7ed67724b3037e25bb65dd
Daniil <ddan62626@gmail.com>
2021-07-13 14:21:03 +0300

    feat: generic typings

--- index.d.ts
+++ index.d.ts
@@ -3,27 +3,32 @@
 import { EventEmitter } from 'events';
 import * as grpc from '@grpc/grpc-js';
 
-type GrpcRequest = any;
+type CallType = 'unary' | 'request_stream' | 'response_stream' | 'duplex';
 
-type GrpcResponse = any;
+type MetadataValue = grpc.MetadataValue;
+type Metadata = Record<string, MetadataValue>;
 
-type GrpcCall =
-  grpc.ServerUnaryCall<any, any> |
-  grpc.ServerReadableStream<any, any> |
-  grpc.ServerWritableStream<any, any> |
-  grpc.ServerDuplexStream<any, any>
+type GrpcRequest<T> = T;
+
+type GrpcResponse<T> = T;
+
+type GrpcCall<Request, Response> =
+  | grpc.ServerUnaryCall<GrpcRequest<Request>, GrpcResponse<Response>>
+  | grpc.ServerReadableStream<GrpcRequest<Request>, GrpcResponse<Response>>
+  | grpc.ServerWritableStream<GrpcRequest<Request>, GrpcResponse<Response>>
+  | grpc.ServerDuplexStream<GrpcRequest<Request>, GrpcResponse<Response>>;
 
 interface App<T> {
-  context: T
+  context: T;
 }
 
-declare class Mali<T> extends EventEmitter implements App<T> {
+declare class Mali<AppContextType> extends EventEmitter implements App<AppContextType> {
   constructor(path?: any, name?: string | ReadonlyArray<string>, options?: any);
   name: string;
   env: string;
   ports: ReadonlyArray<number>;
   silent: boolean;
-  context: T
+  context: AppContextType;
 
   addService (path: any, name: string | ReadonlyArray<string>, options?: any): void;
   use (service?: any, name?: any, fns?: any): void;
@@ -34,53 +39,61 @@ declare class Mali<T> extends EventEmitter implements App<T> {
 }
 
 declare namespace Mali {
-  interface Context<R> {
+  export interface Context<RequestType, ResponseType, AppContextType = any> {
     name: string;
     fullName: string;
     service: string;
     package: string;
-    app: App<R>;
-    call: GrpcCall;
-    request: Request;
-    response: Response;
-    req: GrpcRequest;
-    res: GrpcResponse;
-    type: string;
-    metadata: any;
-    locals: object;
-    get (field: string): any;
-    set (field: any, val?: any): void;
-    sendMetadata (md: any): void;
-    getStatus (field: string): any;
-    setStatus (field: any, val?: any): void;
+    app: App<AppContextType>;
+    call: GrpcCall<RequestType, ResponseType>;
+    request: Request<RequestType, GrpcCall<RequestType, ResponseType>>;
+    response: Response<ResponseType, GrpcCall<RequestType, ResponseType>>;
+    req: Request<RequestType, GrpcCall<RequestType, ResponseType>>['req'];
+    res: Response<ResponseType, GrpcCall<RequestType, ResponseType>>['res'];
+    type: CallType;
+    metadata: Metadata;
+    locals: Record<string, any>;
+
+    set (field: string, val: MetadataValue): void;
+    set (metadata: grpc.Metadata | Metadata): void;
+    get (field: string): MetadataValue;
+    sendMetadata (md: grpc.Metadata | Metadata): void;
+
+    getStatus (field: string): MetadataValue;
+    setStatus (field: string, val: MetadataValue): void;
+    setStatus (metadata: grpc.Metadata | Metadata): void;
   }
 
-  class Request {
-    constructor(call: any, type: string);
-    call: any;
-    type: string;
-    metadata: any;
-    req: GrpcRequest;
+  export class Request<T, Call extends GrpcCall<any, any>> {
+    constructor(call: Call, type: CallType);
+    call: Call;
+    type: CallType;
+    metadata: Metadata;
+    req: GrpcRequest<T>;
 
     getMetadata (): grpc.Metadata;
-    get (field: string): any;
+    get (field: string): MetadataValue;
   }
 
-  class Response {
-    constructor(call: any, type: string);
-    call: any;
-    type: string;
-    metadata: any;
-    status: any;
-    res: GrpcResponse;
-    set (field: any, val?: any): void;
-    get (field: string): any;
+  export class Response<T, Call extends GrpcCall<any, any>> {
+    constructor(call: Call, type: CallType);
+    call: Call;
+    type: CallType;
+    metadata: Metadata;
+    status: Metadata;
+    res: GrpcResponse<T> | Error;
+
+    set (field: string | grpc.Metadata, val: MetadataValue): void;
+    set (metadata: grpc.Metadata | Metadata): void;
+    get (field: string): MetadataValue;
     getMetadata (): grpc.Metadata;
-    sendMetadata (md?: any): void;
-    getStatus (field: string): any;
-    setStatus (field: any, val?: any): void;
-  }
+    sendMetadata (md?: grpc.Metadata | Metadata): void;
 
+    setStatus (field: string, val: MetadataValue): void;
+    setStatus (metadata: grpc.Metadata | Metadata): void;
+    getStatus (field: string): MetadataValue;
+    getStatusMetadata (): grpc.Metadata;
+  }
 }
 
 export = Mali;
